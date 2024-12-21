fbdcfe02a5a864848a1e892ef4773ce8264c89fe
Josh Mangiola <contact@joshmangiola.com>
2024-12-22 00:00:20 +1030

    Removes Complexity from rewriting paths

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,10 +4,6 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	server: {
-		host: '0.0.0.0', // Allow connections from all network interfaces
-		port: 4321, // You can change this port as needed
-	},
 	integrations: [
 		starlight({
 			title: 'Dugong',
